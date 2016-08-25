77f88cd9baea50acde25d1655a8d7aab839ea686
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-08-25 12:14:38 +0700

    style(applayout.js): jshint

--- app/scripts/controllers/applayout.js
+++ app/scripts/controllers/applayout.js
@@ -31,25 +31,24 @@ function ($scope, $mdSidenav, $timeout) {
             'url': '#/signup'
         }
     ];
-    $scope.toggleLeft = buildDelayedToggler('left');
 
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
+    function debounce(func, wait) {
+        var timer;
+        return function debounced() {
+            var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function() {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
     }
     function buildDelayedToggler(navID) {
-      return debounce(function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-      }, 200);
+        return debounce(function() {
+            $mdSidenav(navID).toggle();
+        }, 200);
     }
+
+    $scope.toggleLeft = buildDelayedToggler('left');
 }]);
