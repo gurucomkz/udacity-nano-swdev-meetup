c7324633f0bb0e8aea6f8ff48b10a3cd6e0f2f7a
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-08-26 13:26:14 +0700

    event creation checkpoint

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * @ngdoc function
  * @name appMeetupApp.controller:CreateCtrl
@@ -10,8 +8,75 @@
 angular.module('appMeetupApp')
 .controller('CreateCtrl', [
     '$scope',
-function ($scope) {
+    'Friends',
+    'Events',
+    '$mdDialog',
+    'Accounts',
+function ($scope, Friends, Events, $mdDialog, Accounts) {
     'use strict';
+    
+    $scope.event = {
+        name: '',
+        type: '',
+        host: '',
 
+    };
+    $scope.eventTypes = Events.getEventTypes();
+
+    $scope.contacts = [];
+    $scope.allContacts = Friends.list();
+
+    $scope.openFriendsList = function(ev){
+        $mdDialog.show({
+            scope: $scope,        // use parent scope in template
+            preserveScope: true,
+            controller: 'FriendsdialogCtrl',
+            templateUrl: 'views/friendsdialog.tpl.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: true
+        })
+        .then(function(listToAdd) {
+            for(var x in listToAdd) {
+                $scope.contacts.push(listToAdd[x]);
+            }
+        });
+    };
+
+    $scope.querySearch = function(query){
+        var result = [];
+        query = query.toLowerCase();
+        for(var x in $scope.allContacts) {
+            var candidate = $scope.allContacts[x];
+            if(candidate && candidate.name){
+                if(candidate.name.toLowerCase().indexOf(query)>=0){
+                    result.push(candidate);
+                }
+            }
+        }
+        return result;
+    };
+
+    $scope.myName = Accounts.current().name;
+    $scope.meAsHost = false;
+    $scope.toggleMeAsHost = function(){
+        $scope.meAsHost = !$scope.meAsHost;
+    };
+    $scope.hostSearchText = '';
+
+    $scope.hostQuerySearch = function(query){
+        var result = [];
+        query = query.toLowerCase();
+        for(var x in $scope.allContacts) {
+            var candidate = $scope.allContacts[x];
+            if(candidate && candidate.name){
+                if(candidate.name.toLowerCase().indexOf(query)>=0){
+                    result.push(candidate.name);
+                }
+            }
+        }
+        return result;
+    };
 
 }]);
--- app/scripts/services/events.js
+++ app/scripts/services/events.js
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * @ngdoc service
  * @name appMeetupApp.Events
@@ -8,16 +6,49 @@
  * Factory in the appMeetupApp.
  */
 angular.module('appMeetupApp')
-  .factory('Events', function () {
-    // Service logic
-    // ...
+.factory('Events', function () {
+    'use strict';
 
-    var meaningOfLife = 42;
-
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
+    var eventTypes = {
+        birthday: 'Birthday party',
+        conference: 'Conference talk',
+        wedding: 'Wedding',
+        boardGame: 'Board Game'
     };
-  });
+    var events;
+
+    function _getEvents(){
+        if(!localStorage.events){
+            localStorage.events = JSON.stringify([false]);
+        }
+
+        events = JSON.parse(localStorage.events);
+        if(!events){
+            events = [false];
+        }
+    }
+
+    function _saveEvents(){
+        localStorage.events = JSON.stringify(events);
+    }
+
+    _getEvents();
+    return {
+        create: function(event){
+            if(!event) {
+                return false;
+            }
+            event.id = event.length;
+            events.push(event);
+            _saveEvents();
+
+            return true;
+        },
+        list: function(){
+            return events;
+        },
+        getEventTypes: function () {
+            return eventTypes;
+        }
+    };
+});
